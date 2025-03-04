48efd59ad042869e253424bdf0582551ebaef956
Liqu1d-G <1643076551@qq.com>
2025-03-04 18:14:25 +0000

    release mask-dpo

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 [![license](https://img.shields.io/github/license/InternLM/opencompass.svg)](./LICENSE)
 
-This is the repository for our ANAH series of papers, containing [ANAH: Analytical Annotation of Hallucinations in Large Language Models](https://arxiv.org/abs/2405.20315) and [ANAH-v2: Scaling Analytical Hallucination Annotation of Large Language Models](https://arxiv.org/abs/2407.04693). 
+This is the repository for our ANAH series of papers, which aims to reduce hallucinations in LLM through research involving benchmarking, detection, and mitigation of hallucinations:
+
+- **[Benchmark]** [ANAH: Analytical Annotation of Hallucinations in Large Language Models](https://arxiv.org/abs/2405.20315)
+- **[Detection]** [ANAH-v2: Scaling Analytical Hallucination Annotation of Large Language Models](https://arxiv.org/abs/2407.04693)
+- **[Mitigation]** [Mask-DPO: Generalizable Fine-grained Factuality Alignment of LLMs]()
 
 The repo contains:
 
 + The [data](#huggingface-dataset) for training and evaluating the LLM which consists of sentence-level hallucination annotations.
 + The [model](#huggingface-model) for annotating the hallucination.
 + The [code](#evaluation) for evaluating the hallucinations level of LLM-generated content and the LLMs' ability to annotate hallucination.
-
++ The [code](#maskdpo-training) for performing fine-grained factuality alignment.
 
 ## 🚀 What's New
+- **[2025.01.23]** Mask-DPO has been accepted by the ICLR 2025. 🎉🎉🎉
 - **[2024.09.26]** ANAH-v2 has been accepted by the NeurIPS 2024. 🎉🎉🎉
 - **[2024.07.12]** ANAH-v2 [Annotator](https://huggingface.co/opencompass/anah-v2) has been open-sourced. 🔥🔥🔥
 - **[2024.07.03]** ANAH [Annotator-7B](https://huggingface.co/opencompass/anah-7b) & [20B](https://huggingface.co/opencompass/anah-20b) have been open-sourced.  🔥🔥🔥
@@ -46,10 +51,24 @@ Through iterative self-training, we simultaneously and progressively scale up th
 
 The final dataset encompasses both over ∼3k topics, ∼196k model responses, and ∼822k annotated sentences, in English and Chinese.
 
+The final hallucination annotator (detector) with only 7B parameters surpasses the performance of GPT-4 and obtains new state-of-the-art hallucination detection results on HaluEval and HalluQA by zero-shot inference.
+
 <p align="center">
   <img src="docs/figure/teaser-v2.jpg" height="500">
 </p>
 
+## Mask-DPO: Generalizable Fine-grained Factuality Alignment of LLMs
+
+[![arXiv](https://img.shields.io/badge/arXiv--b31b1b.svg)]()
+
+Mask-DPO is a fine-grained factuality alignment method based on Direct Preference Optimization (DPO).
+
+Incorporating sentence-level factuality as mask signals, Mask-DPO only learns from factually correct sentences in the preferred samples and prevents the penalty on factual contents in the not preferred samples, which resolves the ambiguity in the preference learning.
+
+
+<p align="center">
+  <img src="docs/figure/maskdpo.png" height="300">
+</p>
 
 ## 🤗 HuggingFace Model & Dataset
 <a name="huggingface-dataset"></a>
@@ -147,9 +166,64 @@ python -u ./eval/anah_v1/eval.py \
     --eval_sorce_path {your_evaluation_result_path} \
 ```
 
+<a name="maskdpo-training"></a>
+## 🚄 Factuality Alignment Tutorial
+
+### 1. Install Dependencies
+
+Mask-DPO utilizes [XTuner](https://github.com/InternLM/xtuner) as the training engine.
+
+```bash
+conda env create -f maskdpo.yml 
+```
+
+### 2. Prepare Fine-grained Preference Data
+
+You need to prepare sentence-level facutality preference data in the following format:
+
+```json
+{
+    "prompt": [{"role": "user", "content": "..."}],
+    "chosen": [{"role": "assistant", "content": "..."}],
+    "chosen_item": {
+        "sents": ["sent1", "sent2", "..."],
+        "type": ["hallucination", "no_hallucination", "..."]
+    },
+    "rejected": [{"role": "assistant", "content": "..."}],
+    "rejected_item": {
+        "sents": ["sent1", "sent2", "..."],
+        "type": ["hallucination", "no_hallucination", "..."]
+    }
+}
+```
+
+where `chosem_item` is the fine-grained factual information about `chosem`, `sents` is a sentence-level slice of the `content` in `chosen`, and `type` is the hallucination situation of the corresponding sentence.
+`rejected_item` is the same.
+
+We recommend you to use [ANAH-v2](https://huggingface.co/opencompass/anah-v2) for fine-grained hallucination annotation of your data. Of course you can use other methods as well.
+
+
+### 3. Training
+
+After putting the data and initial model paths to the corresponding locations in config, you can use the following command to train the model.
+
+```bash
+python -m torch.distributed.run \
+  --nproc_per_node=4 \
+  --nnodes=1 \
+  --node_rank=0 \
+  --rdzv_id=1234 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=127.0.0.1:1234 \
+  ./maskdpo/train.py \
+  ./maskdpo/example_config.py \
+  --deepspeed deepspeed_zero3 \
+  --launcher pytorch
+```
+
 ## ❤️ Acknowledgements
 
-ANAH is built with [InternLM](https://github.com/InternLM/InternLM) and [LMDeploy](https://github.com/InternLM/lagent). Thanks for their awesome work!
+ANAH is built with [InternLM](https://github.com/InternLM/InternLM), [Xtuner](https://github.com/InternLM/xtuner) and [LMDeploy](https://github.com/InternLM/lagent). Thanks for their awesome work!
 
 ## 🖊️ Citation
 
@@ -167,6 +241,12 @@ If you find this project useful in your research, please consider citing:
   author={Gu, Yuzhe and Ji, Ziwei and Zhang, Wenwei and Lyu, Chengqi and Lin, Dahua and Chen, Kai},
   journal={arXiv preprint arXiv:2407.04693},
   year={2024}
+}
+
+@inproceedings{gumask,
+  title={Mask-DPO: Generalizable Fine-grained Factuality Alignment of LLMs},
+  author={Gu, Yuzhe and Zhang, Wenwei and Lyu, Chengqi and Lin, Dahua and Chen, Kai},
+  booktitle={The Thirteenth International Conference on Learning Representations}
 }
 ```
 
---
+++ maskdpo/model.py
@@ -0,0 +1,169 @@
+from copy import deepcopy
+
+import torch
+import torch.distributed as dist
+import torch.nn.functional as F
+from mmengine import MessageHub
+from transformers.integrations import is_deepspeed_zero3_enabled
+
+from xtuner.parallel.sequence import (gather_forward_split_backward,
+                                      get_sequence_parallel_group,
+                                      get_sequence_parallel_world_size,
+                                      split_for_sequence_parallel)
+from xtuner.model.dpo import DPO
+
+class MaskDPO(DPO):
+    
+    def compute_loss(self, data, data_samples=None):
+        # modified from https://github.com/huggingface/trl/blob/main/trl/trainer/dpo_trainer.py  # noqa
+        # shift labels first and add a dummy label at the end, to support sequence parallel  # noqa
+        data['labels'] = torch.cat(
+            (data['labels'][:, 1:], torch.zeros_like(data['labels'][:, :1])),
+            dim=1)
+        tmp_label = data['labels'].clone()
+        tmp_label[tmp_label == 0] = -100
+        all_loss_mask = data[
+            'labels'] != -100  # loss mask of all tokens in all sp ranks  # noqa
+
+        labels = data['labels'].clone()
+
+        neg_indices = torch.where((labels < -100000))
+        zero_indices = torch.where((labels == -100) | (labels == 0))
+        pos_indices = torch.where(labels > 0)
+
+        labels[neg_indices] += 1000000
+        labels[zero_indices] = -100
+
+        loss_mask = torch.zeros_like(labels)
+        loss_mask[pos_indices] = 1
+        loss_mask[neg_indices] = -1
+
+        all_loss_mask = loss_mask.clone()
+        data['labels'] = labels
+        
+        if get_sequence_parallel_world_size() > 1:
+            data = self._split_for_sequence_parallel(data)
+
+        all_logits = self.llm(**data).logits
+        with torch.no_grad():
+            if self.ref_llm is None:
+                with self.llm.disable_adapter():
+                    all_ref_logits = self.llm(**data).logits
+            else:
+                all_ref_logits = self.ref_llm(**data).logits
+
+        labels = data['labels']
+        labels[labels == -100] = 0
+        # loss_mask = labels != 0  # loss mask in a single sp rank
+        policy_logps = self._gather_masked_logits(all_logits, labels,
+                                                  loss_mask)
+        ref_logps = self._gather_masked_logits(all_ref_logits, labels,
+                                               loss_mask)
+
+        if get_sequence_parallel_world_size() > 1:
+            policy_logps = gather_forward_split_backward(
+                policy_logps,
+                dim=1,
+                sp_group=get_sequence_parallel_group(),
+                grad_scale='up')
+            ref_logps = gather_forward_split_backward(
+                ref_logps,
+                dim=1,
+                sp_group=get_sequence_parallel_group(),
+                grad_scale='up')
+
+        if not self.use_varlen_attn:
+            (policy_chosen_logps, policy_rejected_logps,
+             reference_chosen_logps,
+             reference_rejected_logps) = self.get_logps(
+                 policy_logps, ref_logps, all_loss_mask)
+        else:
+            message_hub = MessageHub.get_instance('varlen_attn_args')
+            rank = dist.get_rank()
+            cu_seqlens = message_hub.get_info(f'cumulative_len_rank_{rank}')
+            (policy_chosen_logps, policy_rejected_logps,
+             reference_chosen_logps,
+             reference_rejected_logps) = self.get_var_len_atten_logps(
+                 policy_logps, ref_logps, all_loss_mask, cu_seqlens,
+                 data['attention_mask'])
+
+        pi_logratios = policy_chosen_logps - policy_rejected_logps
+        ref_logratios = reference_chosen_logps - reference_rejected_logps
+
+        logits = pi_logratios - ref_logratios
+        if self.loss_type == 'sigmoid':
+            loss = (-F.logsigmoid(self.beta * logits) *
+                    (1 - self.label_smoothing) -
+                    F.logsigmoid(-self.beta * logits) * self.label_smoothing)
+        elif self.loss_type == 'robust':
+            loss = (-F.logsigmoid(self.beta * logits) *
+                    (1 - self.label_smoothing) +
+                    F.logsigmoid(-self.beta * logits) *
+                    self.label_smoothing) / (1 - 2 * self.label_smoothing)
+        elif self.loss_type == 'hinge':
+            loss = torch.relu(1 - self.beta * logits)
+        elif self.loss_type == 'ipo':
+            # eqn (17) of the paper where beta is the regularization
+            # parameter for the IPO loss, denoted by tau in the paper.  # noqa
+            loss = (logits - 1 / (2 * self.beta))**2
+        elif self.loss_type == 'kto_pair':
+            # eqn (7) of the HALOs paper
+            chosen_KL = (policy_chosen_logps -
+                         reference_chosen_logps).mean().clamp(min=0)
+            rejected_KL = (policy_rejected_logps -
+                           reference_rejected_logps).mean().clamp(min=0)
+
+            chosen_logratios = policy_chosen_logps - reference_chosen_logps
+            rejected_logratios = \
+                policy_rejected_logps - reference_rejected_logps
+            # As described in the KTO report, the KL term for chosen (rejected)
+            # is estimated using the rejected (chosen) half.  # noqa
+            loss = torch.cat(
+                (
+                    1 - F.sigmoid(self.beta *
+                                  (chosen_logratios - rejected_KL)),
+                    1 - F.sigmoid(self.beta *
+                                  (chosen_KL - rejected_logratios)),
+                ),
+                0,
+            )
+        elif self.loss_type == 'sppo_hard':
+            # In the paper (https://arxiv.org/pdf/2405.00675),
+            # SPPO employs a soft probability approach,
+            # estimated using the PairRM score. The probability calculation
+            # is conducted outside of the trainer class.
+            # The version described here is the hard probability version,
+            # where P in Equation (4.7) of Algorithm 1 is set to 1 for
+            # the winner and 0 for the loser.
+            a = policy_chosen_logps - reference_chosen_logps
+            b = policy_rejected_logps - reference_rejected_logps
+
+            loss = (a - 0.5 / self.beta)**2 + (b + 0.5 / self.beta)**2
+        elif self.loss_type == 'nca_pair':
+            chosen_rewards = (policy_chosen_logps -
+                              reference_chosen_logps) * self.beta
+            rejected_rewards = (policy_rejected_logps -
+                                reference_rejected_logps) * self.beta
+            loss = (-F.logsigmoid(chosen_rewards) -
+                    0.5 * F.logsigmoid(-chosen_rewards) -
+                    0.5 * F.logsigmoid(-rejected_rewards))
+        else:
+            raise ValueError(
+                f'Unknown loss type: {self.loss_type}. Should be one of '
+                "['sigmoid', 'hinge', 'ipo', 'kto_pair', "
+                "'sppo_hard', 'nca_pair', 'robust']")
+        # for logging
+        chosen_rewards = self.beta * (
+            policy_chosen_logps - reference_chosen_logps)
+        rejected_rewards = self.beta * (
+            policy_rejected_logps - reference_rejected_logps)
+        reward_acc = (chosen_rewards > rejected_rewards).float().mean()
+
+        loss_dict = {
+            'loss': loss,
+            'chosen_rewards': chosen_rewards.mean(),
+            'rejected_rewards': rejected_rewards.mean(),
+            'reward_acc': reward_acc,
+            'reward_margin': (chosen_rewards - rejected_rewards).mean(),
+        }
+        return loss_dict
