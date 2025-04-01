3193cf33069bc8b4d646838443b250219dd3921f
Yuzhe Gu <74947293+Liqu1d-G@users.noreply.github.com>
2025-04-01 17:22:20 +0800

    [Fix]: fix bugs in anah-v2 (#13)
    * fix bugs in eval script and enviroment

--- eval/anah_v2/eval.py
+++ eval/anah_v2/eval.py
@@ -1,19 +1,20 @@
 import os
-import subprocess
 import argparse
 import csv
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
+from lmdeploy import TurbomindEngineConfig, pipeline, ChatTemplateConfig
 
 from utils import get_lines_from_path, write_lines_to_path, sentence_tokenize
 from anahv2_prompt import fact_check_prompt, reference_check_prompt, hallucination_check_prompt
 
-def process_question(model, tokenizer, question: str, sentence: str, document: str, language: str) -> str:
+
+def process_question(pipe, question: str, sentence: str, document: str, language: str) -> str:
     """Process the question through multiple steps: fact-checking, reference-checking, hallucination-checking."""
+    
     # Step 1: Fact-checking
     fact_check = fact_check_prompt(question, sentence, language)
     messages = [{"role": "user", "content": fact_check}]
-    response, _ = model.chat(tokenizer, messages)
+    response = pipe(messages).text
 
     if response == "<No Facts>" or response == "<无事实>":
         return "nofact"
@@ -23,7 +24,7 @@ def process_question(model, tokenizer, question: str, sentence: str, document: s
     # Step 2: Reference-checking
     reference_check = reference_check_prompt(question, document, sentence, language)
     messages.append({"role": "user", "content": reference_check})
-    response, _ = model.chat(tokenizer, messages)
+    response = pipe(messages).text
 
     response_tmp = response.strip().replace(" ", "").lower()
     if "noreferenceinformation" in response_tmp or "无参考信息" in response_tmp:
@@ -35,7 +36,7 @@ def process_question(model, tokenizer, question: str, sentence: str, document: s
     # Step 3: Hallucination-checking
     hallucination_check = hallucination_check_prompt(question, reference, sentence, language)
     messages.append({"role": "user", "content": hallucination_check})
-    response, _ = model.chat(tokenizer, messages)
+    response = pipe(messages).text
 
     hallucination_type = response.strip().replace(" ", "").lower()
     if "nohallucination" in hallucination_type or "无幻觉" in hallucination_type:
@@ -80,9 +81,9 @@ def evaluate_output(args):
 
 def run(args):
     path = 'opencompass/anah-v2'
-    tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=torch.float16, trust_remote_code=True).cuda()
-    model.eval()
+    backend_config = TurbomindEngineConfig(model_name="internlm2", tp=1)
+    chat_template_config = ChatTemplateConfig(model_name='internlm2')
+    pipe = pipeline(path, backend_config=backend_config, chat_template_config=chat_template_config)
 
     old_lines = get_lines_from_path("eval/anah_v2/question_document.jsonl")
     ref_lines = dict()
@@ -91,14 +92,14 @@ def run(args):
     
     lines = get_lines_from_path(args.json_path)
     new_lines = []
-    for line in lines:
+    for line in tqdm(lines, desc="processing"):
         response = line["response"]
         question = line["question"]
         language = ref_lines[question]["language"]
         document = ref_lines[question]["document"]
         sentences = sentence_tokenize(response, language, keep_end=False)
         for sent in sentences:
-            annotation = process_question(model, tokenizer, question, sent, document, language)
+            annotation = process_question(pipe, question, sent, document, language)
             new_lines.append({"question": question, "response": response, "sentence": sent, "annotation": annotation, "language": language})
 
     write_lines_to_path(args.annotation_path, new_lines)
